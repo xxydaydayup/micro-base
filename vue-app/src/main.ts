9fd1822cc5f24073ae2ca3b0452c22110177422c
Xu <982792003@qq.com>
2023-04-13 12:49:45 +0800

    feat  vue & viteconfig

--- vue-app/src/main.ts
+++ vue-app/src/main.ts
@@ -1,26 +1,30 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import { renderWithQiankun, qiankunWindow } from 'vite-plugin-qiankun/dist/helper';
-import router from './router'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import {
+  renderWithQiankun,
+  qiankunWindow,
+} from "vite-plugin-qiankun/dist/helper";
+import router from "./router";
 let app: any;
+//3.修改vue的入口文件，和react一样，判断是否是qiankun环境，来决定render的方式
 if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
-  createApp(App).use(router).mount('#app');
+  createApp(App).use(router).mount("#app");
 } else {
   renderWithQiankun({
     mount(props) {
       app = createApp(App);
-      app.use(router).mount(props.container.querySelector('#app'));
+      app.use(router).mount(props.container.querySelector("#app"));
     },
     bootstrap() {
-      console.log('vue app bootstrap');
+      console.log("vue app bootstrap");
     },
     update() {
-      console.log('vue app update');
+      console.log("vue app update");
     },
     unmount() {
-      console.log('vue app unmount');
+      console.log("vue app unmount");
       app?.unmount();
-    }
+    },
   });
 }
--- vue-app/vite.config.ts
+++ vue-app/vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import qiankun from 'vite-plugin-qiankun';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
+//1.下载依赖vite-plugin-qiankun
+import qiankun from "vite-plugin-qiankun";
+//2.配置base，使用插件
 export default defineConfig({
-  base: '/sub-vue', // 和基座中配置的activeRule一致
+  base: "/sub-vue", // 与基座中配置的activeRule一致
   server: {
     port: 3002,
     cors: true,
-    origin: 'http://localhost:3002'
+    origin: "http://localhost:3002", //根据其服务的端口而变
   },
   plugins: [
     vue(),
-    qiankun('sub-vue', { // 配置qiankun插件
-      useDevMode: true
-    })
-  ]
-})
+    qiankun("sub-vue", {
+      // 配置qiankun插件
+      useDevMode: true,
+    }),
+  ],
+});
