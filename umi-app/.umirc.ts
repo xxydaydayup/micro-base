1e92d3ecdc59db64c8cb9170fdab3671a7c22fa1
Xu <982792003@qq.com>
2023-04-13 13:08:01 +0800

    feat umi & config

--- umi-app/.umirc.ts
+++ umi-app/.umirc.ts
@@ -1,3 +1,7 @@
+//1.安装npm i @umijs/plugins  qiankun的umi插件
+//2.配置.umirc.ts 2步就可以接入主应用基座了
+//如果想用生命周期，可以在入口文件qpp.ts引入
+
 export default {
   base: '/sub-umi',
   npmClient: 'npm',
--- umi-app/src/app.ts
+++ umi-app/src/app.ts
@@ -1,11 +1,12 @@
+//3.如果想在qiankun的生命周期中做些处理，需要修改下入口文件
 export const qiankun = {
   async mount(props: any) {
-    console.log(props)
+    console.log(props);
   },
   async bootstrap() {
-    console.log('umi app bootstraped');
+    console.log("umi app bootstraped");
   },
   async afterMount(props: any) {
-    console.log('umi app afterMount', props);
+    console.log("umi app afterMount", props);
   },
 };
